042d25d8065ab40af619cc14f51ab2982413383d
John Gerhardt <jag@aiglon.ch>
2022-01-16 21:24:49 +0100

    Creating the Stack Navigator

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView} from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
 
+// add the stak navigator as this is going to be the main navigation of our app
+const Stack = createStackNavigator();
+
+// Global device setup
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+// Here is the main applicatoin
 export default function App() {
+  const [userData, setUserData] = useState("");
+
+
+
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Estates Home" component={HomeScreen} params={,null}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
+
+
+function HomeScreen({route, navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style = "dark"  />
+      <SafeAreaView style={styles.mainView}>
+        <ScrollView style={{height:"100%", backgroundColor: "#ccc"}}>
+        <Text>
+           {userData === "" ? null : userData}
+         </Text>
+        </ScrollView>
+      </SafeAreaView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mainView: {
+    backgroundColor: '#ccc',
+    width: "100%",
+    height: "100%",
+    justifyContent: 'flex-end',
+  },
+  tableCell : {
+    height : "290",
+  }
 });
